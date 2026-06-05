8a3830e24e3de2879f66a3729595c1ca3af7750b
ZhangJiaqi <2018653737@qq.com>
2026-06-05 21:03:48 +0800

    Update 242_isAnagram.py

--- LeetCode-Practice/Easy/242_isAnagram.py
+++ LeetCode-Practice/Easy/242_isAnagram.py
@@ -1,3 +1,7 @@
+#题目：242. 有效的字母异位词
+#难度：Easy
+#日期：2026.6.5
+#思路：用 26 个计数器：s 中出现的字母 +1，t 中出现的字母 -1，最后所有计数器必须为 0
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
